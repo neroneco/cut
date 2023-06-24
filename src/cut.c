2f74c5b465cbb74bf4907c87737a798400608d41
Marcin Szpilski <szpilski.marcin.99@gmail.com>
2023-06-24 15:00:54 +0200

    CUT   - fixing thread status variable,   - making user exit more responsive,   - implementing smaller thread condition variable timeout,   - adding analyzing functions

--- src/cut.c
+++ src/cut.c
@@ -1,11 +1,12 @@
 #define _DEFAULT_SOURCE
+#include <features.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/time.h>
-#include <features.h>
+#include <sys/types.h>
 #include <termios.h>
 #include <time.h>
 #include <signal.h>
@@ -14,12 +15,14 @@
 #include <pthread.h>
 
 #include "queue.h"
+#include "analyzer.h"
+
 
 static void* reader(void* arg);
 static void* analyzer(void* arg);
 static void* printer(void* arg);
 
-static void handler(int num);
+static void exit_handler(int num);
 
 enum THREAD {
     READER = 0,
@@ -50,10 +53,10 @@ int main() {
     //    1) thread set status to 1
     //    2) watchdog resetes status to 0 with 2 sec intervals
     //    3) if status 0 after 2 sec - thread hanged
-    size_t status[3];
+    size_t status[3] = {0};
 
     struct sigaction sa = {0};
-    sa.sa_handler = handler;
+    sa.sa_handler = exit_handler;
 
     // queue init
     init_queue(&q[READER], 100, 3000);
@@ -66,21 +69,23 @@ int main() {
     tcsetattr(STDIN_FILENO, TCSANOW, &new_settings);
     fcntl(STDIN_FILENO, F_SETFL, fcntl(0, F_GETFL) | O_NONBLOCK);
 
+    // creating threads
+    pthread_create(&thr[READER], NULL, reader, status);
+    pthread_create(&thr[ANALYZER], NULL, analyzer, status);
+    pthread_create(&thr[PRINTER], NULL, printer, status);
+
     // seting signals handler
     sigaction(SIGTERM, &sa, NULL);
     sigaction(SIGINT, &sa, NULL);
 
-    // creating threads
-    pthread_create(&thr[READER], NULL, reader, &status[READER]);
-    pthread_create(&thr[ANALYZER], NULL, analyzer, &status[ANALYZER]);
-    pthread_create(&thr[PRINTER], NULL, printer, &status[PRINTER]);
-    // TODO watchdog code
+    // watchdog code; cyclic 2 sec check
     for (size_t i = 1; !end[WATCHDOG] && !(c == 'q'); i++) {
         c = getc(stdin);
-        usleep(1000000u);
+        usleep(100000u);
         printf("Watchdog\n");
         i %= 20;
         if (!i) {
+            printf("2 sec passed\n");
             if (!status[READER]) {
                 printf("Reader hanging\n");
                 end[WATCHDOG] = 1;
@@ -103,13 +108,8 @@ int main() {
     if (c == 'q')
         printf("\nEnding program (user quit)\n");
 
-    // Ending program (normally)
-    end[READER] = 1;
-    end[ANALYZER] = 1;
-    end[PRINTER] = 1;
-    pthread_join(thr[READER], NULL);
-    pthread_join(thr[ANALYZER], NULL);
-    pthread_join(thr[PRINTER], NULL);
+    // Ending program
+    exit_handler(0);
     close(fd);
 
     return 0;
@@ -123,22 +123,23 @@ void* reader(void* arg) {
     int rs;
     ssize_t data_size;
     char data_buffer[4096] = {0};
-    fd = open("/proc/stat", O_RDONLY);
-    if (fd == -1)
-        pthread_exit(status);
 
     while (!end[READER]) {
-        status[READER] = 1;
+        status[READER] = 1ul;
 
-        // TODO read data
+        fd = open("/proc/stat", O_RDONLY);
+        if (fd == -1)
+            pthread_exit(status);
+
+        // read data
         data_size = read(fd, data_buffer, 4000);
         if (data_size == -1)
             pthread_exit(status);
-        printf("Reader: %s\n", data_buffer);
+        //printf("Reader: Data read: %zu\n %s", data_size, data_buffer);
 
         pthread_mutex_lock(&mtx[READER]);
 
-        // TODO enqueue data for analyzer
+        // enqueue data for analyzer
         rs = enqueue(&q[READER], data_buffer, (size_t)data_size);
 
         pthread_cond_signal(&cond_var[READER]);
@@ -151,6 +152,7 @@ void* reader(void* arg) {
             printf("Reader: No data added\n");
         }
 
+        close(fd);
         // FIXME only for testing
         usleep(1000000);
     }
@@ -162,46 +164,72 @@ void* reader(void* arg) {
 static void* analyzer(void* arg) {
     struct timespec ts;
     struct timeval  tv;
+    long timeout = 500l; // thread condition timeout in miliseconds
 
     size_t* status = (size_t*)arg;
 
     int rs = 1;
     ssize_t data_size = 0;
-    char data_buffer[4096] = {0};
+    char data_buffer_raw[4096] = {0};
+
+    // I assume that there will not be more then 30 cores
+    size_t cpus_num = 0;
+    double total = 0;
+    double idle = 0;
+    double cpu_usage[30] = {0};
+    struct cpu_time cpu_new_time[30] = {0};
+    struct cpu_time cpu_old_time[30] = {0};
 
     while (!end[ANALYZER]) {
-        status[ANALYZER] = 1;
+        status[ANALYZER] = 1ul;
+
         pthread_mutex_lock(&mtx[READER]);
 
         // Wait for signal but with 1 sec timeout (just in case if reader hanged or ended)
         gettimeofday(&tv, NULL);
-        ts.tv_sec = tv.tv_sec + 1l;
-        ts.tv_nsec = (tv.tv_usec * 1000l);
+        ts.tv_nsec = (tv.tv_usec * 1000l) + timeout * 1000000; // 500 [ms]
+        ts.tv_sec = tv.tv_sec + (ts.tv_nsec / 1000000000l);
+        ts.tv_nsec %= 1000000000l;
         pthread_cond_timedwait(&cond_var[READER], &mtx[READER], &ts);
 
         // TODO dequeue data from reader
         if (q[READER].size > 0) {
             data_size = (ssize_t)q[READER].head->data_size;
-            rs = dequeue(&q[READER], data_buffer);
+            rs = dequeue(&q[READER], data_buffer_raw);
         } else {
             rs = 1;
         }
         pthread_mutex_unlock(&mtx[READER]);
 
         // TODO analyze data
-
         if (!rs) {
-            printf("Analyzer: %s\n", data_buffer);
+            printf("Analyzer: Got data from Readers queue: %zu\n", data_size);
         } else {
             printf("Analyzer: No data in Readers queue\n");
+            continue;
         }
 
+        // get number of cpus (cpus num is always too big by one)
+        cpus_num = get_cpu_count(data_buffer_raw, data_size);
+        printf("CPUs number: %zu\n", cpus_num);
+        if (cpus_num < 2)
+            pthread_exit(status);
+
+        // get new cpu idle/total time data and compare it with previous one 
+        // after that determine cpu_usage
+        analyze(cpu_new_time, cpus_num ,data_buffer_raw, data_size);
+        for (size_t i = 0; i < cpus_num; i++) {
+            idle = (double)(cpu_new_time[i].idle - cpu_old_time[i].idle);
+            total = (double)(cpu_new_time[i].total - cpu_old_time[i].total);
+            cpu_usage[i] = ((total - idle) / total) * 100.0;
+        }
+        memcpy(cpu_old_time, cpu_new_time, sizeof(struct cpu_time) * cpus_num);
 
 
         pthread_mutex_lock(&mtx[ANALYZER]);
 
         // TODO enqueue data for printer
-        rs = enqueue(&q[ANALYZER], data_buffer, (size_t)data_size);
+        rs = enqueue(&q[ANALYZER], cpu_usage, (size_t)(cpus_num * sizeof(double)));
 
         pthread_cond_signal(&cond_var[ANALYZER]);
         pthread_mutex_unlock(&mtx[ANALYZER]);
@@ -223,46 +251,60 @@ static void* analyzer(void* arg) {
 static void* printer(void* arg) {
     struct timespec ts;
     struct timeval  tv;
+    long timeout = 500l; // thread condition timeout in miliseconds
 
     size_t* status = (size_t*)arg;
 
     int rs = 1;
     ssize_t data_size = 0;
-    char data_buffer[4096] = {0};
+    size_t cpus_num = 0;
+    double cpu_usage[30] = {0};
 
     while (!end[PRINTER]) {
-        status[PRINTER] = 1;
+        status[PRINTER] = 1ul;
+
         pthread_mutex_lock(&mtx[ANALYZER]);
 
         // Wait for signal but with 1 sec timeout (just in case if reader hanged or ended)
         gettimeofday(&tv, NULL);
-        ts.tv_sec = tv.tv_sec + 1l;
-        ts.tv_nsec = (tv.tv_usec * 1000l);
+        ts.tv_nsec = (tv.tv_usec * 1000l) + timeout * 1000000; // 500 [ms]
+        ts.tv_sec = tv.tv_sec + (ts.tv_nsec / 1000000000l);
+        ts.tv_nsec %= 1000000000l;
         pthread_cond_timedwait(&cond_var[ANALYZER], &mtx[ANALYZER], &ts);
 
         // TODO dequeue data from analyzer
         if (q[ANALYZER].size > 0) {
             data_size = (ssize_t)q[ANALYZER].head->data_size;
-            rs = dequeue(&q[ANALYZER], data_buffer);
+            rs = dequeue(&q[ANALYZER], cpu_usage);
         } else {
             rs = 1;
         }
 
         pthread_mutex_unlock(&mtx[ANALYZER]);
 
-        // TODO print data to stdin
         if (!rs) {
-            printf("Printer: %s\n", data_buffer);
+            printf("Printer: Got data from Analyzers queue: %zu\n", data_size);
         } else {
             printf("Printer: No data in queue\n");
+            continue;
         }
+
+        // TODO print data to stdin
+        cpus_num = ((size_t)data_size / sizeof(double));
+        for (size_t i = 0; i < cpus_num; i++) {
+            if (!i)
+                printf("\tcpu overal usage: %.3f%%\n", cpu_usage[i]);
+            else
+                printf("\tcpu%zu: %.3f%%\n", i, cpu_usage[i]);
+        }
+
     }
     pthread_exit(status);
 }
 
 
-
-static void handler(int sig) {
+// TODO make it as default exit handler
+static void exit_handler(int sig) {
     if (sig == SIGINT)
         write(1, "\nEnding program (interrupted SIGINT)\n", 38);
     if (sig == SIGTERM)
